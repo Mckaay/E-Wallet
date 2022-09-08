c7a1c1143e7b80bc0e3b4fa826a7400f48b6512e
Mckay <michal1nycz@gmail.com>
2022-09-08 09:38:14 +0200

    Show/Hide password script and show total balance of period

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-menu')[0]
+const eyeToggler = document.querySelector('#toggler');
+const passwordInput = document.querySelector('#password');
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
+eyeToggler.addEventListener('click', function () {
+  if (this.className === "icon-eye") {
+    this.className = "icon-eye-off";
+  }
+  else {
+    this.className = "icon-eye";
+  }
+  changeType();
 })
+
+function changeType() {
+  let passwordInput = document.querySelector('#password');
+  if (eyeToggler.className === 'icon-eye') {
+    passwordInput.type = 'password';
+  }
+  else {
+    passwordInput.type = 'text';
+  }
+}
+
+
+
+
+
+
+
+
+
